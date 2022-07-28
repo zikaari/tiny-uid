4e7d72b51c3b3b49c76d0f9231ea6fbd909aed6f
Neek Sandhu <neek.sandhu@outlook.com>
2022-07-28 12:38:53 -0700

    fix typings

--- index.d.ts
+++ index.d.ts
@@ -4,5 +4,5 @@
  * @param length - Length of the generated string (default 7)
  * @param hex - set to true if returned value should be a hexadecimal based string (default false)
  */
-declare function uid(length?: number, hex?: boolean);
-export default uid;
+declare function uid(length?: number, hex?: boolean): string
+export default uid
